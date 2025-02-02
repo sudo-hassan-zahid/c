e5bfecc8a0794e7be8fba64cadfabd4f44c99abc
codeofhassan <theultimatehassan@gmail.com>
2025-02-03 00:25:19 +0500

    Moved to CLion

---
+++ cmake-build-debug/CMakeFiles/untitled1.dir/main.c.obj.d
@@ -0,0 +1,12 @@
+CMakeFiles/untitled1.dir/main.c.obj: H:\My\ Projects\C-Language\main.c \
+ E:/Node/CLION/CLion\ 2024.3.2/bin/mingw/x86_64-w64-mingw32/include/stdio.h \
+ E:/Node/CLION/CLion\ 2024.3.2/bin/mingw/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ E:/Node/CLION/CLion\ 2024.3.2/bin/mingw/x86_64-w64-mingw32/include/corecrt.h \
+ E:/Node/CLION/CLion\ 2024.3.2/bin/mingw/x86_64-w64-mingw32/include/_mingw.h \
+ E:/Node/CLION/CLion\ 2024.3.2/bin/mingw/x86_64-w64-mingw32/include/_mingw_mac.h \
+ E:/Node/CLION/CLion\ 2024.3.2/bin/mingw/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ E:/Node/CLION/CLion\ 2024.3.2/bin/mingw/x86_64-w64-mingw32/include/vadefs.h \
+ E:/Node/CLION/CLion\ 2024.3.2/bin/mingw/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ E:/Node/CLION/CLion\ 2024.3.2/bin/mingw/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ E:/Node/CLION/CLion\ 2024.3.2/bin/mingw/x86_64-w64-mingw32/include/swprintf.inl \
+ E:/Node/CLION/CLion\ 2024.3.2/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdio_s.h
