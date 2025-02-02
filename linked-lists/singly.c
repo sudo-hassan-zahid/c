b2e5ad9e71d0bd412a60667311e086806d07c20b
codeofhassan <theultimatehassan@gmail.com>
2025-02-03 00:31:39 +0500

    Create code for nodes

--- linked-lists/singly.c
+++ linked-lists/singly.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+typedef struct{
+  int data;
+  struct node* next;
+} Node;
+
+Node* insert(Node* head, int data){
+  Node* temp = (Node*)malloc(sizeof(Node));
+  temp->data = data;
+  temp->next = NULL;
+  if(head == NULL){
+    head = temp;
+  }
+  return temp;
+}
 
 int main(){
-    
-    return 0;
+
+   return 0;
 }
